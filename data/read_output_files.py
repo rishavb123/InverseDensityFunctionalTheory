ef5f3eb9d675f846db242eb69d8b7aa5459a127d
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-03-29 17:40:27 -0400

    documented read output file module

--- data/read_output_files.py
+++ data/read_output_files.py
@@ -4,10 +4,24 @@ import numpy as np
 class ReadOutputFiles():
 
     def __init__(self, path) -> None:
+        """Initializes the ReadOutputFiles class
+
+        Args:
+            path (str): The folder path that includes all the sparc files
+        """
         self.path = path
 
     @staticmethod
     def __read_csv_file(f_path, flattened=False):
+        """Reads a csv file into rows
+
+        Args:
+            f_path (str): The csv file path
+            flattened (bool, optional): Whether or not the tensor in the csv file has been flattened or not. Defaults to False.
+
+        Returns:
+            list: The list of rows
+        """
         rows = []
         with open(f_path) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
@@ -40,6 +54,14 @@ class ReadOutputFiles():
 
     @staticmethod
     def __convert_rows_file_to_np(rows):
+        """Converts the list of rows into a numpy array
+
+        Args:
+            rows (list): The list of rows
+
+        Returns:
+            np.array: The numpy array
+        """
         lengths = [
             1 + max(row[i] for row in rows)
             for i in range(3)
@@ -50,23 +72,57 @@ class ReadOutputFiles():
         return arr
     
     def __read_csv_file_np(self, f_name, flattened=False):
+        """Reads a csv file and returns it in an numpy array
+
+        Args:
+            f_name (str): The file name (relative to the instance variable self.path)
+            flattened (bool, optional): Whether the csv tensor is flattened. Defaults to False.
+
+        Returns:
+            np.array: The numpy array
+        """
         rows = ReadOutputFiles.__read_csv_file(f"{path}/{f_name}", flattened=flattened)
         arr = ReadOutputFiles.__convert_rows_file_to_np(rows)
         return arr
 
     def get_converged_exc_density(self):
+        """Gets the converged exc density
+
+        Returns:
+            np.array: The converged exc density
+        """
         return self.__read_csv_file_np("Converged_exc_density.csv")
     
     def get_feature_0(self):
+        """Gets the feature 0 file
+
+        Returns:
+            np.array: The feature 0 file
+        """
         return self.__read_csv_file_np("feature_0_spin_type_0.csv")
     
     def get_feature_1(self):
+        """Gets the feature 1 file
+
+        Returns:
+            np.array: The feature 1 file
+        """
         return self.__read_csv_file_np("feature_1_spin_type_0.csv")
     
     def get_hsmp_iter_0(self):
+        """Gets the hsmp feature at iteration 0
+
+        Returns:
+            np.array: The hsmp feature at iteration 0
+        """
         return self.__read_csv_file_np("HSMP_iter_0_spin_0_SH_1_STEP_0.500000_RCUT_0.500000.csv")
     
     def get_xc_potential(self):
+        """Gets the converged xc potential
+
+        Returns:
+            np.array: The converged xc potential
+        """
         return self.__read_csv_file_np("xc_potential.csv", flattened=True)
 
 if __name__ == '__main__':
