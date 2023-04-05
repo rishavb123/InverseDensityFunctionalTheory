1a68608a3c75c7b663eb668f95c87bfae5d3f921
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-04 21:46:41 -0400

    ran black

--- data/read_output_files.py
+++ data/read_output_files.py
@@ -1,8 +1,10 @@
+import os
+
 import csv
 import numpy as np
 
-class ReadOutputFiles():
 
+class ReadOutputFiles:
     def __init__(self, path) -> None:
         """Initializes the ReadOutputFiles class
 
@@ -24,29 +26,28 @@ class ReadOutputFiles():
         """
         rows = []
         with open(f_path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
+            csv_reader = csv.reader(csv_file, delimiter=",")
             for row in csv_reader:
                 if flattened:
                     if row[0].isdigit():
-                        rows.append((
-                            int(row[0]),
-                            float(row[1])
-                        ))
+                        rows.append((int(row[0]), float(row[1])))
                 else:
                     if row[0].isdigit() and row[1].isdigit() and row[2].isdigit():
-                        rows.append((
-                            int(row[0]),
-                            int(row[1]),
-                            int(row[2]),
-                            float(row[3])
-                        ))
-        
+                        rows.append(
+                            (int(row[0]), int(row[1]), int(row[2]), float(row[3]))
+                        )
+
         if flattened:
             full_length = max(row[0] for row in rows)
-            dim_length = int(np.round(full_length ** (1/3)))
+            dim_length = int(np.round(full_length ** (1 / 3)))
             sq_dim_length = dim_length * dim_length
             rows = [
-                (row[0] % dim_length, (row[0] % sq_dim_length) // dim_length, row[0] // sq_dim_length, row[1])
+                (
+                    row[0] % dim_length,
+                    (row[0] % sq_dim_length) // dim_length,
+                    row[0] // sq_dim_length,
+                    row[1],
+                )
                 for row in rows
             ]
 
@@ -62,15 +63,12 @@ class ReadOutputFiles():
         Returns:
             np.array: The numpy array
         """
-        lengths = [
-            1 + max(row[i] for row in rows)
-            for i in range(3)
-        ]
+        lengths = [1 + max(row[i] for row in rows) for i in range(3)]
         arr = np.empty(lengths)
         for row in rows:
             arr[row[0], row[1], row[2]] = row[3]
         return arr
-    
+
     def __read_csv_file_np(self, f_name, flattened=False):
         """Reads a csv file and returns it in an numpy array
 
@@ -92,7 +90,7 @@ class ReadOutputFiles():
             np.array: The converged exc density
         """
         return self.__read_csv_file_np("Converged_exc_density.csv")
-    
+
     def get_feature_0(self):
         """Gets the feature 0 file
 
@@ -100,7 +98,7 @@ class ReadOutputFiles():
             np.array: The feature 0 file
         """
         return self.__read_csv_file_np("feature_0_spin_type_0.csv")
-    
+
     def get_feature_1(self):
         """Gets the feature 1 file
 
@@ -108,15 +106,17 @@ class ReadOutputFiles():
             np.array: The feature 1 file
         """
         return self.__read_csv_file_np("feature_1_spin_type_0.csv")
-    
+
     def get_hsmp_iter_0(self):
         """Gets the hsmp feature at iteration 0
 
         Returns:
             np.array: The hsmp feature at iteration 0
         """
-        return self.__read_csv_file_np("HSMP_iter_0_spin_0_SH_1_STEP_0.500000_RCUT_0.500000.csv")
-    
+        return self.__read_csv_file_np(
+            "HSMP_iter_0_spin_0_SH_1_STEP_0.500000_RCUT_0.500000.csv"
+        )
+
     def get_xc_potential(self):
         """Gets the converged xc potential
 
@@ -125,9 +125,10 @@ class ReadOutputFiles():
         """
         return self.__read_csv_file_np("xc_potential.csv", flattened=True)
 
-if __name__ == '__main__':
-    path = '/storage/home/hcoda1/6/rbhagat8/data/sparc_runs/H2O/72834_-1.0--1.0--1.0_1.0-1.0-0.0_-1.0-0.0-1.0'
-    # path = "C:/Users/risha/Downloads/sparc_data"
+
+if __name__ == "__main__":
+    # path = '/storage/home/hcoda1/6/rbhagat8/data/sparc_runs/H2O/72834_-1.0--1.0--1.0_1.0-1.0-0.0_-1.0-0.0-1.0'
+    path = "C:/Users/risha/Downloads/sparc_data"
     reader = ReadOutputFiles(path)
     arr = reader.get_converged_exc_density()
 
